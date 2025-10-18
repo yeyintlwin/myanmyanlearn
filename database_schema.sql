11ca88755fb6e52b8c01638a6d8e48b6588b4279
Ye Yint Lwin <yeyintlwin.dev@gmial.com>
2025-10-18 21:20:08 +0900

    fix: correct database schema export - only members and roles tables

--- database_schema.sql
+++ database_schema.sql
@@ -16,31 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `authorities`
---
-
-DROP TABLE IF EXISTS `authorities`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `authorities` (
-  `username` varchar(50) NOT NULL,
-  `authority` varchar(50) NOT NULL,
-  KEY `username` (`username`),
-  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `authorities`
---
-
-LOCK TABLES `authorities` WRITE;
-/*!40000 ALTER TABLE `authorities` DISABLE KEYS */;
-INSERT INTO `authorities` VALUES ('admin','ROLE_USER'),('admin','ROLE_ADMIN');
-/*!40000 ALTER TABLE `authorities` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `members`
 --
 
@@ -63,31 +38,6 @@ LOCK TABLES `members` WRITE;
 /*!40000 ALTER TABLE `members` DISABLE KEYS */;
 INSERT INTO `members` VALUES ('admin','{bcrypt}$2a$10$hLRtXyCL2Gso8EFl61SRTu21aMHjkbVs7SqxdQFsJcxrUr3d3LMaW',1),('employee','{bcrypt}$2a$10$hLRtXyCL2Gso8EFl61SRTu21aMHjkbVs7SqxdQFsJcxrUr3d3LMaW',1),('manager','{bcrypt}$2a$10$hLRtXyCL2Gso8EFl61SRTu21aMHjkbVs7SqxdQFsJcxrUr3d3LMaW',1);
 /*!40000 ALTER TABLE `members` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `persistent_logins`
---
-
-DROP TABLE IF EXISTS `persistent_logins`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `persistent_logins` (
-  `username` varchar(64) NOT NULL,
-  `series` varchar(64) NOT NULL,
-  `token` varchar(64) NOT NULL,
-  `last_used` timestamp NOT NULL,
-  PRIMARY KEY (`series`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `persistent_logins`
---
-
-LOCK TABLES `persistent_logins` WRITE;
-/*!40000 ALTER TABLE `persistent_logins` DISABLE KEYS */;
-/*!40000 ALTER TABLE `persistent_logins` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -114,31 +64,6 @@ LOCK TABLES `roles` WRITE;
 INSERT INTO `roles` VALUES ('admin','ROLE_ADMIN'),('employee','ROLE_EMPLOYEE'),('manager','ROLE_MANAGER');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `enabled` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('admin','$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVEFDi',1);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -149,4 +74,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-18 21:15:50
+-- Dump completed on 2025-10-18 21:19:36
